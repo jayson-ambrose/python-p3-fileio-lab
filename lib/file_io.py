841e1838577eaae64753fca25672f3f64281d4f4
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-03-20 22:28:28 -0600

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,21 @@
+import ipdb
+
 def write_file(file_name, file_content):
+
+    file_thing = (file_name + ".txt")
+    with open(file_thing, mode='w', encoding='utf-8') as text_file:
+        text_file.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    file_thing = (file_name + ".txt")
+    with open(file_thing, mode='a', encoding='utf-8') as text_file:
+        text_file.write(append_content)
     pass
 
 def read_file(file_name):
+
+    with open(file_name + ".txt", encoding='utf-8') as text_file:
+        return(text_file.read())
     pass
+
